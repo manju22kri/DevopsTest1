696833e3e61453cba2c690e731287b10933a20fb
manju22kri <manju22kri@gmail.com>
2023-07-14 22:17:43 +0530

    new changes

--- TestTest.java
+++ TestTest.java
@@ -1,12 +1,22 @@
-import static org.junit.jupiter.api.Assertions.*;
+package Java_interview;
 
-import org.junit.jupiter.api.Test;
+import java.io.IOException;
 
-class TestTest {
+public class Check_Vowel {
+	public static void main(String[] args) throws IOException {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+		String name = "Manjunath";
+		String str = name.toLowerCase();
+		int count = 0;
+		for (int i = 0; i < name.length(); i++) {
+			if (str.charAt(i) == 'a'||  str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u')
+			{		
+				count++;
+			}
+		}
+		System.out.println(count);
 	}
-
 }
+
+
+
